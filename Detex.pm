a8bff24d196e04162db409b42adef10a4273e688
wikenator <arnold.wikey@gmail.com>
2017-05-09 12:41:39 -0700

    prevent paren removal around negative number arguments for functions

--- Detex.pm
+++ Detex.pm
@@ -211,7 +211,7 @@ sub detex {
 	$detexExpr = &injectAsterixes($detexExpr, $debug);
 
 	# final paren removal for negative numbers
-	$detexExpr =~ s/([^\^])\((-\w+)\)([^\^])/$1$2$3/g;
+	$detexExpr =~ s/([^\^($search_terms)])\((-\w+)\)([^\^])/$1$2$3/g;
 	$detexExpr =~ s/^\((-\w+)\)([^\^])/$1$2/;
 
 	if ($debug) { print STDERR "iterations used: $infinite\n"; }
